103b478d9b557f1c602f6d21399895473d64b7e8
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-10-10 11:33:59 +0600

    Created get all data api and single phone api

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const phones = require('./phones.json');
 const app = express();
 const port = 5001;
 
 app.get('/', (req, res) => {
   res.send('my phone information coming soon ton moon');
+});
+
+app.get('/phones', (req, res) => {
+  res.send(phones);
+});
+
+app.get('/phones/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log('i need data for id', id);
+  const phone = phones.find((phone) => phone.id === id) || {};
+  res.send(phone);
 });
 
 app.listen(port, () => {
